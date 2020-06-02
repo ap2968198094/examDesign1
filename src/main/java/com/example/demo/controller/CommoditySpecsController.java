1502e3c91a0ce3bb38d3247e576e4f31c304de40
18773227890 <2968198094@qq.com>
2020-06-02 17:24:46 +0800

    课程设计

--- src/main/java/com/example/demo/controller/CommoditySpecsController.java
+++ src/main/java/com/example/demo/controller/CommoditySpecsController.java
@@ -1,15 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.CommoditySpecsService;
+import com.example.demo.util.ResultVoUtil;
+import com.example.demo.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("commoditySpecs")
+@RequestMapping("/commoditySpecs")
 @ResponseBody
 public class CommoditySpecsController {
     @Autowired
     private CommoditySpecsService commoditySpecsService;
+
+    @GetMapping("/findAll")
+    public ResultVo findAll(){
+        return ResultVoUtil.success(commoditySpecsService.findAll());
+    }
 }
